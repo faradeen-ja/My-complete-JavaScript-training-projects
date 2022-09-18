68d491328d6eaf44e900537e6e48ba9784bdf545
Faradeen Jami <93830144+faradeen-ja@users.noreply.github.com>
2022-09-18 09:36:35 -0500

    Update script.js

---
+++ Advanced JavaScript A-Z/01-JS Fundamentals-Part-1/script.js
@@ -0,0 +1,15 @@
+//VS code shortcuts working on JS
+
+//CTLR + D
+//select a text area and use this short cut to select all similar texts and update or delete 
+
+//Alt + arrow up or down
+//move text/code line up or down
+
+//alt + shift + a
+//comment out multi line
+
+//alt + shift + arrow down
+//copy line of code down
+
+//CTRL + /  single line commnet
