88db62f4686734c409109ce5ddb3e11e875c1488
Faradeen Jami <93830144+faradeen-ja@users.noreply.github.com>
2022-10-16 09:30:53 -0500

    Update script.js

--- Advanced JavaScript A-Z/01-JS Fundamentals-Part-1/script.js
+++ Advanced JavaScript A-Z/01-JS Fundamentals-Part-1/script.js
@@ -388,3 +388,11 @@ drink2 = 'water';
 
 console.log(drink2) 
 
+
+////////////////////////////////////
+a quick challenge on ternary operator and template litrals usecase
+
+const bill = 275;
+const tip = bill <= 300 && bill >= 50 ? bill * 0.15 : bill * 0.2;
+console.log(`the bill was ${bill}, the tip was ${tip}, and the total value is ${bill + tip}');
+
