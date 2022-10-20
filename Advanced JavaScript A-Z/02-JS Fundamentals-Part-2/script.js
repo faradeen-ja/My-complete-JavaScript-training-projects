66fec60432d3193442fe342cc211bb3d1ebd72e6
Faradeen Jami <93830144+faradeen-ja@users.noreply.github.com>
2022-10-19 23:16:57 -0500

    Update script.js

--- Advanced JavaScript A-Z/02-JS Fundamentals-Part-2/script.js
+++ Advanced JavaScript A-Z/02-JS Fundamentals-Part-2/script.js
@@ -23,3 +23,11 @@ function fruiteProcessor(apples, oranges) {
   return juice;
 }
 
+
+
+// We run, invoke or call the fruiteProcessor function with 2 params or parameters args or arguments apples and oranges
+// we used those values to make the juice variable 
+const mixJuice = fruiteProcessor(10, 2);
+console.log(mixJuice); // mix our juice with 10 apples and 2 oranges 
+// We give values of 10 apples and 2 oranges to our paramters in intial function fruiteProcessor and store it in a variable called mixjuice
+// We can then invoke it, (the function) by running console.log(mixJuice) to console.
