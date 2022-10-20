335eb9967b0c15e7a1e654c51c73d77bb9256b0b
Faradeen Jami <93830144+faradeen-ja@users.noreply.github.com>
2022-10-19 23:09:59 -0500

    Update script.js

--- Advanced JavaScript A-Z/02-JS Fundamentals-Part-2/script.js
+++ Advanced JavaScript A-Z/02-JS Fundamentals-Part-2/script.js
@@ -5,3 +5,21 @@
 // Using a variable, without declaring it, is not allowed:
 'use strict';
 x = 3.14; // will throw an error
+
+
+
+///////////////////////////// ///
+//Functions
+
+function logger() {
+  console.log('My name is ____');
+}
+// We can invoke, call or run a function like this
+logger();
+
+
+function fruiteProcessor(apples, oranges) {
+  const juice = `Juice with ${apples} apples and ${oranges} oranges`;
+  return juice;
+}
+
