e4037206a0c486225448dda05c5f0e0e19bf1968
Faradeen Jami <93830144+faradeen-ja@users.noreply.github.com>
2022-10-19 23:03:02 -0500

    Update script.js

--- Advanced JavaScript A-Z/01-JS Fundamentals-Part-1/script.js
+++ Advanced JavaScript A-Z/01-JS Fundamentals-Part-1/script.js
@@ -396,3 +396,14 @@ const bill = 275;
 const tip = bill <= 300 && bill >= 50 ? bill * 0.15 : bill * 0.2;
 console.log(`the bill was ${bill}, the tip was ${tip}, and the total value is ${bill + tip}');
 
+
+
+//////////////////////////////////
+//PART 2 JS Fundamentals
+
+// Using a variable, without declaring it, is not allowed:
+'use strict';
+x = 3.14; // will throw an error
+
+
+
