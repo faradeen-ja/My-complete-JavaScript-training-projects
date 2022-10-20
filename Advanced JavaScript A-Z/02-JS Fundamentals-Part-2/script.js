116b43e41f45bd02ebc39a3cc213e40e438f6ae3
Faradeen Jami <93830144+faradeen-ja@users.noreply.github.com>
2022-10-19 23:21:43 -0500

    Update script.js

--- Advanced JavaScript A-Z/02-JS Fundamentals-Part-2/script.js
+++ Advanced JavaScript A-Z/02-JS Fundamentals-Part-2/script.js
@@ -22,12 +22,30 @@ function fruiteProcessor(apples, oranges) {
   const juice = `Juice with ${apples} apples and ${oranges} oranges`;
   return juice;
 }
-
-
-
 // We run, invoke or call the fruiteProcessor function with 2 params or parameters args or arguments apples and oranges
 // we used those values to make the juice variable 
 const mixJuice = fruiteProcessor(10, 2);
 console.log(mixJuice); // mix our juice with 10 apples and 2 oranges 
 // We give values of 10 apples and 2 oranges to our paramters in intial function fruiteProcessor and store it in a variable called mixjuice
 // We can then invoke it, (the function) by running console.log(mixJuice) to console.
+
+
+// e.g. of a built if funciton is ...
+const num = Number(11); 
+
+
+////////////////////////////////////
+//4 Types of functions
+
+//1. Function declaration type 
+function calcAge1(birthYear) {
+  return 2090 - birthYear;
+}
+const age1 = calcAge1(1991);
+console.log(age1);
+
+
+
+
+
+
