fa33fcc89cdc54206631f0f383c3f42ec7cba530
Faradeen Jami <93830144+faradeen-ja@users.noreply.github.com>
2022-10-29 23:02:24 -0500

    Add files via upload

---
+++ Advanced JavaScript A-Z/03-Developer-Skills/starter/script.js
@@ -0,0 +1,13 @@
+// Remember, we're gonna use strict mode in all scripts now!
+"use strict";
+
+
+const box = 10;
+console.log('box is empty')
+
+const testFunc = '555';
+if (x === 222){
+   console.log(222) 
+}
+
+const fuckYou = yes => 2 - yes;
