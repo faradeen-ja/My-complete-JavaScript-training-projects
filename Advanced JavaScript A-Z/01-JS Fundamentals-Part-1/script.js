eb91b4d87c89a2888abd0d2e4644ccf568ae0c04
Faradeen Jami <93830144+faradeen-ja@users.noreply.github.com>
2022-11-02 23:22:35 -0500

    Update script.js

--- Advanced JavaScript A-Z/01-JS Fundamentals-Part-1/script.js
+++ Advanced JavaScript A-Z/01-JS Fundamentals-Part-1/script.js
@@ -53,14 +53,14 @@ cosole.log(dogsAges[0]) // 2
 
 
 
-////////////////////////Data Types//////////////////////////////
-///1; - [Objects]
-///2 - [Primitives]
-/// -----Primitives------
-///1 - [Number] floating point numbers
-///2 - [Strings] like sentence word characters or texts 
-///3 - [boolean] are used to make decesions  true or false 
-///4 - [undefined] value in a variable is not defined, set or stored, empty value or parameters 
+////////////////////////📅Data Types//////////////////////////////
+///1; - [Objects] ⬅️
+///2 - [Primitives] ⬅️
+/// -----Primitives------ 👻
+///1 - [Number] floating point numbers 🔢
+///2 - [Strings] like sentence word characters or texts 📝
+///3 - [boolean] are used to make decesions  true or false 👍🏻 👎🏻
+///4 - ⛔[undefined] value in a variable is not defined, set or stored, empty value or parameters 
 ///5 - [Null] also means empty values
 ///6 - [Symbols #$@%] 
 ///7 - [BigInt Bigintergers] long mathimatics numbers
